b93e33e4fb3ab27abaf6db9d68eef8a44395b16d
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-15 01:09:29 -0500

    shellpractice

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,38 +5,37 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
 Y="\e[33m"
+N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGO_HOST="mongodb.devopswithabhi.online"
-MYSQL_HOST="mysql.devopswithabhi.online"
+MYSQL_HOST=mysql.daws88s.online
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
+
 mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>> $LOGS_FILE
-VALIDATE $? "Adding RabbitMQ repository"
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added RabbitMQ repo"
 
-dnf install rabbitmq-server -y &>> $LOGS_FILE
+dnf install rabbitmq-server -y &>>$LOGS_FILE
 VALIDATE $? "Installing RabbitMQ server"
 
-systemctl enable rabbitmq-server &>> $LOGS_FILE
-VALIDATE $? "Enabling RabbitMQ service"
+systemctl enable rabbitmq-server &>>$LOGS_FILE
+systemctl start rabbitmq-server
+VALIDATE $? "Enabled and started rabbitmq"
 
-systemctl start rabbitmq-server &>> $LOGS_FILE
-VALIDATE $? "Starting RabbitMQ service"
-
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-VALIDATE $? "Creating RabbitMQ user and setting permissions"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
+VALIDATE $? "created user and given permissions"
